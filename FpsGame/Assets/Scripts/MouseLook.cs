40201f5560c87204f242bd1efe8e952a3a3729ac
OneShinyBo1 <august.abel.vian.bakker@gmail.com>
2021-09-24 12:01:02 +0200

    Movement

--- FpsGame/Assets/Scripts/MouseLook.cs
+++ FpsGame/Assets/Scripts/MouseLook.cs
@@ -6,14 +6,18 @@ public class MouseLook : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
+
     public GameObject player;
+
     public Transform playerBody;
 
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = player.GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -23,6 +27,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
